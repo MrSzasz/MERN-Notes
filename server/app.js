504ebe7fe057268d69e679a31ad1627559ecf02d
MrSzasz <lugoleandro@hotmail.com>
2023-05-02 14:18:33 -0300

    graphql added

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+    ApolloServer
+} from "@apollo/server"
+import {
+    expressMiddleware
+} from "@apollo/server/express4";
+import http from "http";
+import cors from "cors";
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    const app = express();
+    const httpServer = http.createServer(app);
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+
+    await server.start()
+
+    app.use("/graphql", cors(), express.json(), expressMiddleware(server))
+
+    await new Promise(resolve => httpServer.listen({
+        port: 4000
+    }, resolve))
+
+    console.log("server ready")
+}
+
+export default startApolloServer
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import startApolloServer from "./app.js";
+import { resolvers } from "./graphql/resolvers.js";
+import { typeDefs } from "./graphql/typeDefs.js";
+
+startApolloServer(typeDefs, resolvers)
